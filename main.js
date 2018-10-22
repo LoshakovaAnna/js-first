353cc8a1e1cc5cfe83f7f76fca8fbe8b83297934
Shimchik <ann.loshakova.111@gmail.com>
2018-10-22 21:43:29 +0300

    count the number of words in a line

--- main.js
+++ main.js
@@ -1,30 +1,40 @@
-/*let userPassword=prompt('Введите пароль');
-const originPassword='qwer1234';
 
-if (userPassword===originPassword){
-    alert('ok');
+let arr=prompt('Введите строку');
+var pos = 0;
+var last_pos=-5;
+var count=0;
+
+var target=' ';
+while (true) {
+    var foundPos = arr.indexOf(target, pos);
+    if (foundPos == -1) break;
+
+    if (foundPos==0){  //start space
+        last_pos=foundPos-1;       
+    }
+
+    if (last_pos!==(foundPos-1)){//checking adjacent spaces
+            count++;
+    }
+
+    pos = foundPos + 1; // continue search from next
+    last_pos=foundPos; 
 }
-else{
-    alert('wrong passwod');
-}*/
-/*
-let numberOne=prompt('Введите первое число');
+    //checking if space was before last word
+    if (last_pos<(arr.length-1)){
+        count++;
+    }
+    //no space, word exist
+    if ((count===0)&&(arr.length>0)){
+        
+        count=1;
+    }
+    //checking string  consisting of spaces only
+   var just_spac=true;
+    for(var i=0; i<arr.length;i++){
+        if (arr.charAt(i)!=' ')  
+            just_spac=false;
+    }
+    if (just_spac) count=0;
 
-let numberTwo=prompt('Введите второе число');
-//let numberThree=+numberOne + (+numberTwo);
-let numberThree=parseInt(numberOne) + parseInt(numberTwo);
-alert(numberThree);
-*/
-/*
-let arr=[1,2,3];
-for (let i = 0; i < arr.length; i++) {    
-    console.info(parseInt(arr[i])+1);   
-    
-}*/
-let arr=prompt('input array').split(' ');
-
-
-arr.forEach(arr => {
-    if (!isNaN(parseInt(arr)))
-    console.info(parseInt(arr)+1);   
-});
+alert('всего слов '+count);
